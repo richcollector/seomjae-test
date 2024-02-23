da0143ecc30af958a9212cb55f28d7226e1cfcad
richcollector <ktw9115@naver.com>
2024-02-23 17:00:07 +0900

    style: styled-components 및 Layout 설정 GlobalStyle, ServerStyleSheet, favicon

---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,38 @@
+import { ReactNode } from 'react';
+import styled from 'styled-components';
+
+export default function Layout({ children }: { children: ReactNode }) {
+	return (
+		<Container>
+			<Wrapper>{children}</Wrapper>
+		</Container>
+	);
+}
+
+const Container = styled.div`
+	width: 100%;
+	height: 100%;
+
+	display: flex;
+	flex-direction: column;
+	justify-content: baseline;
+	align-items: center;
+
+	background-color: #f0f1f3;
+`;
+
+const Wrapper = styled.div`
+	width: 1200px;
+
+	display: flex;
+	flex-direction: column;
+	justify-content: baseline;
+	align-items: center;
+
+	padding: 2rem 0;
+
+	@media (max-width: 1200px) {
+		padding: 2rem 1rem;
+		width: 100%;
+	}
+`;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,14 @@
 import type { AppProps } from 'next/app';
+import GlobalStyle from '@/common/styles/GlobalStyle';
+import Layout from '@/components/layout/Layout';
 
 export default function App({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<GlobalStyle />
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
+		</>
+	);
 }
